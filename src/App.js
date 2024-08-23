e8d0727034b31c8794b155ea42fc3b8db7d913b4
Jack Adamczyk <prepinavest@gmail.com>
2024-08-23 09:55:10 -0400

    working with props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import Navbar from "./Navbar";
-import Home from "./Home";
+import Home2 from "./Home2";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <div className="content">
-        <Home />
+        <Home2 />
       </div>
     </div>
   );
---
+++ src/Home2.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { useState } from "react";
+import "./index.css";
+import BlogList from "./BlogList";
+
+export default function Home2() {
+  const [blogs, setBlogs] = useState([
+    { title: "My new site", body: "lorem ipsum...", author: "mario", id: 1 },
+    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
+    {
+      title: "Web dev top tips",
+      body: "lorem ipsum...",
+      author: "mario",
+      id: 3,
+    },
+  ]);
+  return (
+    <div className="home">
+      <BlogList blogs={blogs} title="All Blogs!" />
+    </div>
+  );
+}

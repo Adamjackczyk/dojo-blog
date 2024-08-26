22fe88ff935539e4e6217ababe2ff8b49e98abd9
Jack Adamczyk <prepinavest@gmail.com>
2024-08-26 15:05:58 -0400

    Add 404

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navbar from "./Navbar";
 import Home2 from "./Home2";
 import Create from "./Create";
 import BlogDetails from "./BlogDetails";
+import NotFound from "./NotFound";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
             <Route exact path="/" element={<Home2 />} />
             <Route exact path="/create" element={<Create />} />
             <Route path="/blogs/:id" element={<BlogDetails />} />
+            <Route path="*" element={<NotFound />} />
           </Routes>
         </div>
       </div>
--- src/Create.jsx
+++ src/Create.jsx
@@ -1,13 +1,33 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 const Create = () => {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
   const [author, setAuthor] = useState("mario");
+  const [ispending, setIsPending] = useState(false);
+  const history = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setIsPending(true);
+
+    const blog = { title, body, author };
+    fetch("http://localhost:8000/blogs", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blog),
+    }).then(() => {
+      console.log("new blog added");
+      setIsPending(false);
+      history("/");
+    });
+  };
 
   return (
     <div className="create">
       <h2 className="blog">Create</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>Blog Title:</label>
         <input
           type="text"
@@ -26,7 +46,8 @@ const Create = () => {
           <option value="mario">mario</option>
           <option value="yoshi">yoshi</option>
         </select>
-        <button>Add Blog</button>
+        {ispending && <button disabled>Adding Blog...</button>}
+        {!ispending && <button>Add Blog</button>}
         <p>{title}</p>
         <p>{body}</p>
         <p>Author is {author}</p>
